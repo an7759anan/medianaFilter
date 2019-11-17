36f19f7af65975f241cf3dac224cae33c66de22a
aaanikin <aaanikin@diasoft.ru>
2019-11-17 21:27:03 +0500

    Copy project files

---
+++ MedianaFilterService/Service.svc.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.ServiceModel.Activation;
+using Microsoft.ServiceModel.Web;
+using System.Linq;
+using System.Net;
+
+[assembly: ContractNamespace("", ClrNamespace = "MedianaFilterService")]
+
+namespace MedianaFilterService
+{
+    // TODO: Please set IncludeExceptionDetailInFaults to false in production environments
+    [ServiceBehavior(IncludeExceptionDetailInFaults = true), AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed), ServiceContract]
+    public partial class Service
+    {
+        /// <summary>
+        /// Returns data in response to a HTTP GET request with URIs of the form http://<url-for-svc-file>/GetData?window=3&array=2,80,6,3
+        /// </summary>
+        /// <param name="window">window size</param>
+        /// <param name="array">list of array items that are divided by commas</param>
+        /// <returns>mediana filtered data</returns>
+        [WebHelp(Comment = "GetData return mediana filterd data")]
+        [WebGet(UriTemplate = "GetData?window={n}&array={arrayAsString}")]
+        [OperationContract]
+        public ResponseBody GetData(int n, string arrayAsString)
+        {
+            if (n < 0) throw new WebProtocolException(HttpStatusCode.BadRequest, "window cannot be negative", null);
+            TargetArray array = new TargetArray();
+            array.setArrayFromString(arrayAsString);
+            array.filterArray(n);
+            return new ResponseBody()
+            {
+                Value = array.array
+            };
+        }
+    }
+
+    /// <summary>
+    /// Response body structure.
+    /// </summary>
+    public class ResponseBody
+    {
+        public int[] Value { get; set; }
+    }
+}
---
+++ MedianaFilterService/TargetArray.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+
+namespace MedianaFilterService
+{
+    public class TargetArray
+    {
+        public int[] array;
+        private int arraySize;
+        public void setArrayFromString(string arrayAsString)
+        {
+            string[] splittedArray = arrayAsString.Split(',');
+            arraySize = splittedArray.Length;
+            array = new int[splittedArray.Length];
+            for (int i = 0; i < arraySize; i++ )
+            {
+                Int32.TryParse(splittedArray[i], out array[i]);
+    //            array[i]++;
+            }
+        }
+        public string getArrayAsString()
+        {
+            string result = "";
+            for (int i = 0; i < arraySize; i++) result += (i==0? "":",") + array[i];
+            return result;
+        }
+
+        private void fillWindowArray(int pos, int window, ref int[] windowArray)
+        {
+            int leftShift = - window / 2;
+            for (int i = 0; i < window; i++ )
+            {
+                int currentPos = i + pos - window / 2;
+                if (currentPos < 0) currentPos = 0;
+                else if (currentPos >= arraySize) currentPos = arraySize - 1;
+                windowArray[i] = array[currentPos];
+            }
+        }
+
+        public void filterArray(int window)
+        {
+            int[] newArray = new int[arraySize];
+            int[] windowArray = new int[window];
+            for (int i = 0; i < arraySize; i++ )
+            {
+                fillWindowArray(i, window, ref windowArray);
+                Array.Sort(windowArray);
+                newArray[i] = windowArray[window / 2];
+            }
+            array = newArray;
+        }
+    }
+}
